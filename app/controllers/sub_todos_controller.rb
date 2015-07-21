94287ece394b0912acae5ece57d97ce3236d52e9
bryanmillstein <bryanmillstein@gmail.com>
2015-07-21 10:43:19 -0400

    Adds SubTodos controller.

---
+++ app/controllers/sub_todos_controller.rb
@@ -0,0 +1,36 @@
+class SubTodosController < ApplicationController
+  helper_method :current_filter
+
+  def index
+    @sub_todos = SubTodo
+  end
+
+  def create
+    @todo = Todo.find(params[:todo_id])
+
+    @sub_todo = @todo.sub_todos.new(sub_todo_params)
+
+    @sub_todo.save
+  end
+
+  def update
+    @sub_todo = SubTodo.find(params[:id])
+    @sub_todo.update(sub_todo_params)
+  end
+
+  def destroy
+    @sub_todo = SubTodo.find(params[:id])
+    @sub_todo.destroy
+  end
+
+  def toggle
+    @sub_todo = SubTodo.find(params[:id])
+    @sub_todo.toggle!(:completed)
+  end
+
+  private
+
+  def sub_todo_params
+    params.require(:sub_todo).permit(:title, :completed, :todo_id)
+  end
+end
---
+++ test/controllers/sub_todos_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SubTodosControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
