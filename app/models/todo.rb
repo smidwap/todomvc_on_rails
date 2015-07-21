d1bd122de971879d0b00821315b6a452b6cf97ea
bryanmillstein <bryanmillstein@gmail.com>
2015-07-21 10:36:55 -0400

    Adds ToDo has many SubTodos association.

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,6 +2,10 @@ class Todo < ActiveRecord::Base
   scope :completed, -> { where("completed = ?", true) }
   scope :active, -> { where("completed = ?", false) }
 
+  has_many :sub_todos,
+    class_name: 'SubTodo',
+    foreign_key: :todo_id
+
   def title=(title)
     write_attribute(:title, title.strip)
   end
